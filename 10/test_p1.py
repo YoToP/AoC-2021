881dfb8106ade0dcd7c474f48934802be61fbcec
YoToP <yotop@tuta.io>
2021-12-10 11:57:03 +0100

    testing the up puzzels

--- 10/test_p1.py
+++ 10/test_p1.py
@@ -5,6 +5,18 @@ class test_p1(unittest.TestCase):
     def test_example1(self):
         self.assertEqual(solvep1("10/example1.txt"), 26397,
                          'Solution not ok.')
+    def test_up1(self):
+        self.assertEqual(solvep1("10/aoc-2021-day10-up1.txt"), 26448,
+                         'Solution not ok.')
+    def test_up2(self):
+        self.assertEqual(solvep1("10/aoc-2021-day10-up2.txt"), 51474,
+                         'Solution not ok.')
+    def test_up3(self):
+        self.assertEqual(solvep1("10/aoc-2021-day10-up3.txt"), 26340,
+                         'Solution not ok.')
+    def test_up4(self):
+        self.assertEqual(solvep1("10/aoc-2021-day10-up4.txt"), 1314,
+                         'Solution not ok.')
 
 if __name__ == '__main__':
     unittest.main()
--- 10/test_p2.py
+++ 10/test_p2.py
@@ -5,6 +5,17 @@ class test_p1(unittest.TestCase):
     def test_example1(self):
         self.assertEqual(solvep2("10/example1.txt"), 288957,
                          'Solution not ok.')
-
+    def test_up1(self):
+        self.assertEqual(solvep2("10/aoc-2021-day10-up1.txt"), 14841,
+                         'Solution not ok.')
+    def test_up2(self):
+        self.assertEqual(solvep2("10/aoc-2021-day10-up2.txt"), 15282,
+                         'Solution not ok.')
+    def test_up3(self):
+        self.assertEqual(solvep2("10/aoc-2021-day10-up3.txt"), 217388346164004118353258362,
+                         'Solution not ok.')
+    def test_up4(self):
+        self.assertEqual(solvep2("10/aoc-2021-day10-up4.txt"), 187232,
+                         'Solution not ok.')
 if __name__ == '__main__':
     unittest.main()
