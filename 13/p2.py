756b97ccf73c14664a39ff6c4cbf435babb2ce43
YoToP <y.santana.sanz@gmail.com>
2021-12-13 17:25:53 +0100

    remove the 2D defaultDict, use 1D tuple

--- 13/p2.py
+++ 13/p2.py
@@ -1,53 +1,42 @@
 from time import time
-from collections import defaultdict
-from copy import deepcopy
 def solvep2(path):
     with open(path) as f:
         lines = f.readlines()
-    grid = defaultdict(lambda: defaultdict(int))
-    
+    gridlist = []
     bFoldInstructions = False
     for line in lines:
         line = line.replace('\n', '')
         if line == '':bFoldInstructions = True
         elif not bFoldInstructions:
             x, y = line.split(',') #x is to the right, y is down
-            grid[int(y)][int(x)] += 1
+            gridlist.append((int(y),int(x)))
         else:
             coord, foldLinePos = line.split(' ')[2].split('=')
             foldLinePos = int(foldLinePos)
-            if coord == 'y':
-                _copyGrid = deepcopy(grid)
-                for _row, _grid in _copyGrid.items():
-                    if _row > foldLinePos:
-                        for _column, _x in _grid.items(): 
-                            grid[foldLinePos-(_row - foldLinePos)][_column] += 1
-                        del grid[_row]
-                    
-            elif coord == 'x':
-                _copyGrid = deepcopy(grid)
-                for _row, _grid in _copyGrid.items():
-                    for _column, _x in _grid.items(): 
-                        if _column > foldLinePos:
-                            grid[_row][foldLinePos -(_column-foldLinePos)] += 1
-                        if _column > foldLinePos: 
-                            del grid[_row][_column]
+            for i in range(len(gridlist)):
+                y, x = gridlist[i]
+                if coord == 'y':
+                    if y > foldLinePos:
+                        gridlist[i] = (foldLinePos-(y - foldLinePos),x)
+                elif coord == 'x':
+                    if x > foldLinePos:
+                        gridlist[i] = (y,foldLinePos -(x-foldLinePos))
 
     maxRow = 0
     maxColumn = 0
-    for _row, _grid in grid.items():
-        if _row > maxRow:maxRow = _row
-        for _column, _x in _grid.items():
-            if _column > maxColumn:maxColumn = _column
+    for y, x in gridlist:
+        if y > maxRow:
+            maxRow = y
+        if x > maxColumn:
+            maxColumn = x
     stringlines = []
     for i in range(maxRow+1):
         _string = []
         for j in range(maxColumn+1):
             _string.append(' ')
         stringlines.append(_string)
-    for _row, _grid in grid.items():
-        for _column, _x in _grid.items():
-            stringlines[_row][_column] = chr(9608) #unicode for block
+    for y, x in gridlist:
+        stringlines[y][x] = chr(9608) #unicode for block
     
 
     for _string in stringlines:
@@ -56,5 +45,5 @@ def solvep2(path):
 
 if __name__ == '__main__': # Run when this script is not imported by another script(e.g. Unittest)
     start_time = int(round(time() * 1000))
-    solvep2("13/input.txt")
+    solvep2("13/up.txt")
     print("### total run time is %s miliseconds" % (int(round(time() * 1000)) - start_time))
